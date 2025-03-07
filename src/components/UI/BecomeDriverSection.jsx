da645679ded703a4977f5ddace21605b164783de
Prabash <prabhashwijerathna2001@gmail.com>
2025-03-07 20:57:54 +0530

    add modal for become driver compont

--- src/components/UI/BecomeDriverSection.jsx
+++ src/components/UI/BecomeDriverSection.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/become-driver.css";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Modal, ModalHeader, ModalBody, Button } from "reactstrap";
 
 import driverImg from "../../assets/all-images/toyota-offer-2.png";
+import logo from "../../assets/all-images/login/Black and Orange Car Rent Logo.png";
 
 const BecomeDriverSection = () => {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
   return (
       <section className="become__driver">
         <Container>
@@ -22,12 +26,24 @@ const BecomeDriverSection = () => {
                 Join our community of drivers by reaching out to our admin team. They’ll assist you with all the details, including account setup and access.
               </p>
 
-              <button className="btn become__driver-btn mt-4">
+              <button className="btn become__driver-btn mt-4" onClick={toggle}>
                 Become a Driver
               </button>
             </Col>
           </Row>
         </Container>
+
+        {/* Modal */}
+        <Modal isOpen={modal} toggle={toggle}>
+          <ModalHeader toggle={toggle}>Contact Head Office</ModalHeader>
+          <img src={logo} alt="" className="w-50 h-20 mx-auto  mt-3" />
+          <ModalBody>
+            <p>For more information, please contact our head office at:</p>
+            <p><strong>Phone:</strong> +94 760368019</p>
+            <p><strong>Email:</strong> megaCitySri@gmail.com</p>
+            <p><strong>Address:</strong> Colombo City,Western Srilanka</p>
+          </ModalBody>
+        </Modal>
       </section>
   );
 };
